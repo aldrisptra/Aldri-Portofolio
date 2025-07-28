0828fd86398022a534c5e84e682dc6882ebc0ea1
aldri <11231050@student.itk.ac.id>
2025-07-28 22:32:28 +0800

    update styling section exprience

--- src/components/Exprience.jsx
+++ src/components/Exprience.jsx
@@ -1,12 +1,28 @@
 import '../styles/Exprience.css'
+import infinitevol3 from '../assets/infinitevol3.jpg';
 
 function Exprience() {
   return (
     <section id='exprience'>
         <div className='wrapper'>
-            <h3 className='exprience-title'>Pengalaman</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ea beatae. Mollitia facilis ratione est?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, atque? Ex excepturi sed possimus provident? Consequatur, optio expedita! Fugit nihil corporis est reiciendis molestiae asperiores pariatur a eos in, odit consequatur dicta quidem maiores ea necessitatibus tenetur dolorum numquam harum.</p>
+          <h3 className='exprience-title'>Pengalaman</h3>
+
+          <div className='content-flex'>
+            <img src= {infinitevol3} alt="Pengalaman saya" className='infinitevol3-image' />
+
+            <div className='text-content'>
+              <h2>Infinite Vol. 3 - Staf Divisi Lomba</h2>
+              <p>
+                Tanggung jawab saya mencakup:
+                <ul>
+                  <li>Menyusun konsep teknis dan alur lomba.</li>
+                  <li>Berkoordinasi dengan peserta dan juri untuk kelancaran pelaksanaan lomba.</li>
+                  <li>Membantu dalam penjurian, dokumentasi, dan evaluasi akhir kegiatan.</li>
+                </ul>
+                Melalui pengalaman ini, saya belajar bekerja secara tim, mengelola waktu dengan baik, serta meningkatkan kemampuan komunikasi dan problem solving di bawah tekanan acara.
+              </p>
+            </div>
+          </div>
         </div>
     </section>
   )
